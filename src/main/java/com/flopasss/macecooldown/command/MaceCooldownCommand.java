111a9f6ab7c1b28a1145dd3735103faec5172bd1
florishalleriet <florispshalleriet@gmail.com>
2026-05-07 11:44:51 +0200

    refactor: streamline command registration and improve config toggle handling

--- src/main/java/com/flopasss/macecooldown/command/MaceCooldownCommand.java
+++ src/main/java/com/flopasss/macecooldown/command/MaceCooldownCommand.java
@@ -2,12 +2,16 @@ package com.flopasss.macecooldown.command;
 
 import com.flopasss.macecooldown.MaceCooldown;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static net.minecraft.commands.Commands.literal;
 import static net.minecraft.commands.Commands.argument;
@@ -17,7 +21,7 @@ public class MaceCooldownCommand {
         dispatcher.register(literal("maceCooldown")
 
                 // Cooldown length
-                .then(literal("length").requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
+                .then(literal("duration").requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
                         .then(argument("ticks", IntegerArgumentType.integer(0)).suggests((context, builder) -> {
                             // Provide tab-completion suggestions
                             builder.suggest(MaceCooldown.CONFIG.cooldownTicks);
@@ -29,84 +33,45 @@ public class MaceCooldownCommand {
                             MaceCooldown.CONFIG.save();
 
                             ctx.getSource().sendSuccess(
-                                    () -> Component.literal("Length: " + ticks + " ticks"),
+                                    () -> Component.literal("The duration is now " + ticks + " ticks"),
                                     true);
                             return 1;
                         })))
 
-                // Toggle the mod on/off
-                .then(literal("enabled").requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
-                        .then(argument("boolean", StringArgumentType.word()).suggests((context, builder) -> {
-                            // Provide tab-completion suggestions
-                            builder.suggest("true");
-                            builder.suggest("false");
-                            return builder.buildFuture();
-                        }).executes(context -> {
-                            // Parse the input string into a boolean
-                            String boolStr = StringArgumentType.getString(context, "boolean");
-                            boolean bool = Boolean.parseBoolean(boolStr);
-
-                            // Update config value and save
-                            MaceCooldown.CONFIG.enabled = bool;
-                            MaceCooldown.CONFIG.save();
-
-                            // Send feedback to the command executor
-                            context.getSource()
-                                    .sendSuccess(() -> Component.literal("Enabled: " + bool),
-                                            true);
-                            return 1;
-                        })))
-
-                // Toggle onlyPreventSmash
-                .then(literal("onlyPreventSmash").requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
-                        .then(argument("boolean", StringArgumentType.word()).suggests((context, builder) -> {
-                            // Provide tab-completion suggestions
-                            builder.suggest("true");
-                            builder.suggest("false");
-                            return builder.buildFuture();
-                        }).executes(context -> {
-                            // Parse the input string into a boolean
-                            String boolStr = StringArgumentType.getString(context, "boolean");
-                            boolean bool = Boolean.parseBoolean(boolStr);
-
-                            // Update config value and save
-                            MaceCooldown.CONFIG.onlyPreventSmash = bool;
-                            MaceCooldown.CONFIG.save();
-
-                            // Send feedback to the command executor
-                            context.getSource()
-                                    .sendSuccess(
-                                            () -> Component.literal((bool ? "Only smash"
-                                                    : "All mace") + " attacks are prevented whilst on cooldown"),
-                                            true);
-                            return 1;
-                        })))
-
-                // Toggle onlyApplyOnSmash
-                .then(literal("onlyApplyOnSmash").requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
-                        .then(argument("boolean", StringArgumentType.word()).suggests((context, builder) -> {
-                            // Provide tab-completion suggestions
-                            builder.suggest("true");
-                            builder.suggest("false");
-                            return builder.buildFuture();
-                        }).executes(context -> {
-                            // Parse the input string into a boolean
-                            String boolStr = StringArgumentType.getString(context, "boolean");
-                            boolean bool = Boolean.parseBoolean(boolStr);
-
-                            // Update config value and save
-                            MaceCooldown.CONFIG.onlyApplyOnSmash = bool;
-                            MaceCooldown.CONFIG.save();
-
-                            // Send feedback to the command executor
-                            context.getSource()
-                                    .sendSuccess(
-                                            () -> Component.literal((bool ? "Only smash"
-                                                    : "All mace") + " attacks will apply the cooldown"),
-                                            true);
-                            return 1;
-                        })))
+                // Config boolean toggles
+                .then(createConfigToggle("enabled",
+                        val -> MaceCooldown.CONFIG.enabled = val,
+                        val -> "The mod has been " + (val ? "enabled" : "disabled")))
+                .then(createConfigToggle("onlyPreventSmash",
+                        val -> MaceCooldown.CONFIG.onlyPreventSmash = val,
+                        val -> (val ? "Only smash" : "All mace") + " attacks are prevented whilst on cooldown"))
+                .then(createConfigToggle("onlyApplyOnSmash",
+                        val -> MaceCooldown.CONFIG.onlyApplyOnSmash = val,
+                        val -> (val ? "Only smash" : "All mace") + " attacks will apply the cooldown"))
 
         );
+    }
+
+    private static LiteralArgumentBuilder<CommandSourceStack> createConfigToggle(
+            String name,
+            Consumer<Boolean> setter,
+            Function<Boolean, String> messageFactory) {
+
+        return Commands.literal(name)
+                .requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
+                .then(Commands.argument("boolean", BoolArgumentType.bool())
+                        .executes(context -> {
+                            boolean bool = BoolArgumentType.getBool(context, "boolean");
+
+                            // Update config via the setter
+                            setter.accept(bool);
+                            MaceCooldown.CONFIG.save();
+
+                            // Send feedback using the factory
+                            context.getSource().sendSuccess(
+                                    () -> Component.literal(messageFactory.apply(bool)),
+                                    true);
+                            return 1;
+                        }));
     }
 }
